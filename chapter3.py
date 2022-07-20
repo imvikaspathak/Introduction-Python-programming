6c3296f9d6337ec614e89f769ec5f681d039abc7
imvikaspathak <imvikas.ai@gmail.com>
2022-07-20 18:46:37 +0530

    20july2021

---
+++ chapter3.py
@@ -0,0 +1,50 @@
+# cities = ['new york city', 'mountain view', 'chicago', 'los angeles']
+# for city in cities:
+#     print(city)
+# print("Done!")
+
+#range function use 
+
+# i = 0
+# k = 2
+# j = 100
+
+# max =[]
+
+# for a in range(i,j,k):
+#     max.append(a)
+
+# print(len(max))   
+# max= range(1,100,5)
+# # print(max[4])
+# print(len(max))
+
+  
+# names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+# usernames = []
+
+# # write your for loop here
+# for i in names:
+#     i = i.lower().replace(" ", "_")
+#     usernames.append(i)
+# print(usernames)
+
+# for i in range(1,4):
+#     print(i)
+
+
+
+
+
+# number = [10,20,40,60]
+
+# user = int(input("enter a number:"))
+# if user in number: 
+#     print("you number is match")
+# else:
+#     print("sorry try again")
+
+print(list(range(0,-5)))
+print(list(range(4)))
+print(list(range(4,8)))
+print(list(range(4,8)))
